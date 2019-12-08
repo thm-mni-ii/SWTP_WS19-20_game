aa24e01a33744dd00db7e4bf9410f70ed158e5fa
Marc-Schuster <marc_schuster@t-online.de>
2019-12-08 15:39:29 +0100

    add new methods

--- Dixit/Assets/Scripts/GameManager.cs
+++ Dixit/Assets/Scripts/GameManager.cs
@@ -17,11 +17,55 @@ public class GameManager : MonoBehaviour
     {
         allCards = new List<Card>();
         playerList = new List<Player>();
-        playerList.Add(new Player(1,200,2,0,2,"Marc"));
-        playerList.Add(new Player(2, 300, 2, 0, 2, "Tom"));
-        playerList.Add(new Player(3, 300, 2, 0, 2, "Robert"));
-        playerList.Add(new Player(4, 200, 2, 0, 2, "Herzberg"));
-        playerList.Add(new Player(1, 100, 2, 0, 2, "Priefer"));
+        playerList.Add(new Player(1,0,2,0,2,"Marc"));
+        playerList.Add(new Player(2, 0, 2, 0, 2, "Tom"));
+        playerList.Add(new Player(3, 0, 2, 0, 2, "Robert"));
+        //playerList.Add(new Player(4, 0, 2, 0, 2, "Herzberg"));
+        //playerList.Add(new Player(1, 0, 2, 0, 2, "Priefer"));
+        List<Card> voteList = new List<Card>();
+      //   Test ob bei tippen auf richtige Karte punkt everteilt werden
+        allCards.Add(new Card());
+        allCards[0].IsCorrect = true;
+        allCards[0].PlayerObject = null;
+        allCards[0].Answer = "Richtig";
+        allCards[0].cardID = 42;
+        allCards[0].AddPlayerToPlayerGuesses(new Player(2, 0, 2, 0, 2, "Tom"));
+        allCards.Add(new Card());
+        allCards[1].CorrectVotes = 2;
+        allCards[1].IsCorrect = false;
+        allCards[1].PlayerObject = new Player(2, 0, 2, 0, 2, "Tom");
+        allCards[1].Answer = "Richtig";
+        allCards[1].cardID = 1;
+        allCards[1].AddPlayerToPlayerGuesses(new Player(2, 0, 2, 0, 2, "Marc"));
+        allCards.Add(new Card());
+        allCards[2].IsCorrect = false;
+        allCards[2].PlayerObject = new Player(3, 0, 2, 0, 2, "Robert");
+        allCards[2].Answer = "Richtig";
+        allCards[2].cardID = 3;
+        allCards[2].AddPlayerToPlayerGuesses(new Player(3, 0, 2, 0, 2, "Tom"));
+        allCards.Add(new Card());
+        allCards[3].IsCorrect = false;
+        allCards[3].PlayerObject = new Player(1, 0, 2, 0, 2, "Marc");
+        allCards[3].Answer = "Richtig";
+        allCards[3].CorrectVotes = 2;
+        allCards[3].cardID = 3;
+        voteList.Add(new Card());
+        voteList.Add(new Card());
+        voteList.Add(new Card());
+        voteList.Add(new Card());
+        //vote equal test
+
+        //vote test
+        voteList[0].cardID = 1;
+        voteList[0].IsCorrect = false;
+        voteList[0].PlayerObject = playerList[0];
+        //voteList[0].AddPlayerToPlayerGuesses(new Player(1,50,2,0,2,"Marc"));
+        voteList[0].CorrectVotes = 1;
+        voteList[0].AddPlayerToPlayerGuesses(new Player(2, 0, 2, 0,2, "Tom"));
+        voteList[0].AddPlayerToPlayerGuesses(new Player(3, 0, 2, 0,2, "Robert"));
+        RegisterEqualVotes(voteList);
+        RegisterVotes(voteList);
+        Debug.Log("" + allCards[1].PlayerObject.Score);
         numberOfRounds = 0;
         RoundEnd();
 
@@ -72,12 +116,19 @@ public class GameManager : MonoBehaviour
     {
         for (int j = 0; j < votes.Count; j++) {
             for (int i = 0; i< allCards.Count; i++) {
-                if (allCards[i].PlayerObject.playerID == votes[j].PlayerObject.playerID)
+                if (allCards[i].PlayerObject != null && votes[j].PlayerObject!=null && allCards[i].PlayerObject.playerID == votes[j].PlayerObject.playerID )
                 {
                     allCards[i].CorrectVotes = votes[j].CorrectVotes;
                 }
                     }
-        }
+            
+            }
+        /*AUfgabe der Correct VOtes zum Debuggen
+        for (int j = 0; j < allCards.Count; j++)
+        {
+            if(allCards[j].PlayerObject!=null)
+            Debug.Log("Player:" + allCards[j].PlayerObject.PlayerName+" Card :" + j + " CorrectVotes: " + allCards[j].CorrectVotes);
+        }*/
     }
 
     /// <summary>
@@ -90,26 +141,41 @@ public class GameManager : MonoBehaviour
         {
             for (int i = 0; i < allCards.Count; i++)
             {
-                if (allCards[i].PlayerObject.playerID == answer[j].PlayerObject.playerID)
+               
+                if (allCards[i].PlayerObject!=null && answer[j].PlayerObject!=null && allCards[i].PlayerObject.playerID == answer[j].PlayerObject.playerID)
                 {
                     allCards[i].PlayerGuesses = answer[j].PlayerGuesses;
                 }
             }
         }
+        /*
+        Debug.Log("Card :" + 0 + " PlayerGuesses: " + allCards[0].PlayerGuesses.Count);
+
+        for (int j = 0; j < allCards.Count; j++)
+        {
+            if (allCards[j].PlayerObject != null)
+                Debug.Log("Player:" + allCards[j].PlayerObject.PlayerName + " Card :" + j + " PlayerGuesses: " + allCards[j].PlayerGuesses.Count);
+        }*/
+        
         GiveOutPoints();
     }
+    /// <summary>
+    /// 
+    /// Calls the Method BroadCastScoresViaPM.
+    /// </summary>
     void GiveOutPoints()
     {
+
         //for schleife über player cards
-        for(int i = 0; i < allCards.Count; i++)
+        for (int i = 0; i < allCards.Count; i++)
         {
             //wenn weniger als die hälfte der Spieler dafür gestimmt haben das die karte gleich der richtigen card ist
-            if((allCards[i].CorrectVotes <= allCards.Count) && allCards[i].IsCorrect == false)
+            if ((allCards[i].PlayerObject != null && allCards[i].CorrectVotes <= allCards.Count) && allCards[i].IsCorrect == false)
             {
                 //Schleife über alle Player 
                 for (int k = 0; k < playerList.Count; k++)
                 {
-                    if (playerList[k].playerID == allCards[i].PlayerObject.playerID && allCards[i].CorrectVotes <= playerList.Count/2)
+                  if ( playerList[k].playerID == allCards[i].PlayerObject.playerID && allCards[i].CorrectVotes <= playerList.Count / 2)
                     {
                         //schleife über alle Playerguesses
                         for (int j = 0; j < allCards[i].PlayerGuesses.Count; j++)
@@ -119,24 +185,45 @@ public class GameManager : MonoBehaviour
                     }
                 }
             }
-            else if( allCards[i].IsCorrect == true)
+
+        }
+        for (int i = 0; i < allCards.Count; i++)
+        {
+            if (allCards[i].IsCorrect == true)
             {
                 //schleife über playerList
-                for (int j = 0; j < playerList.Count;j ++)
+                for (int j = 0; j < playerList.Count; j++)
                 {
-                    //schleife über playerguesses
-                    for(int k=0; k < allCards[i].PlayerGuesses.Count;k++)
+                    /*doNotIncrease = false;
+                    for (int g = 0; g < noIncreaseScoreList.Count; g++)
                     {
-                        if (playerList[j].playerID == allCards[i].PlayerGuesses[k].playerID)
+                        if (noIncreaseScoreList[j].playerID == playerList[g].playerID)
                         {
-                            playerList[j].Score += 50;
+                            doNotIncrease = true;
+                            Debug.Log("pid:" + noIncreaseScoreList[j].playerID + ":" + doNotIncrease);
+                        }
+                    }*/
+                    //schleife über playerguesses
+                    for (int k = 0; k < allCards[i].PlayerGuesses.Count; k++)
+                    {
+                        if (playerList[j].playerID == allCards[i].PlayerGuesses[k].playerID )
+                        {
+
+                            for(int cardCount = 0; cardCount < allCards.Count;cardCount++)
+                            {
+                                if(allCards[cardCount].PlayerObject != null && allCards[i].PlayerGuesses[k].playerID==allCards[cardCount].PlayerObject.playerID  && allCards[cardCount].CorrectVotes< playerList.Count/2)
+                                    playerList[j].Score += 50;
+
+                            }
+                            //schleif über karten
+
                         }
                     }
-                   
+
                 }
             }
-        }
-
+    }
+      // BroadCastScoresViaPM();
 
     }
 
@@ -154,13 +241,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 
+    /// Calls the Method RoundEnd()
+    /// </summary>
     public void BroadCastScoresViaPM()
     {
         pm.BoradCastAnswers(playerList);
         RoundEnd();
     }
 
-
+    /// <summary>
+    /// 
+    /// </summary>
     void RoundEnd()
     {
         if(questionSet.QuestionList.Count == 0 || numberOfRounds<=0 )
@@ -214,6 +307,9 @@ public class GameManager : MonoBehaviour
         {
             NextQuestion();
         }
+
+
+
 
         void NextQuestion()
         {
