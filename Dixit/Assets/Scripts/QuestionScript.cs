afe33c3a61f07b7209d9bb2efe6fbba7593b170a
Robert Feuerhack <r.feuerhack@gmx.de>
2019-12-09 21:27:05 +0100

    ready to merge

--- Dixit/Assets/Scripts/QuestionScript.cs
+++ Dixit/Assets/Scripts/QuestionScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class QuestionScript : MonoBehaviourS
+public class QuestionScript : MonoBehaviour
 {
    
     public Text question;
