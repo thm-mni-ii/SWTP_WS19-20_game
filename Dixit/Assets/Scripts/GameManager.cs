aa372191984b78d99d6d1b59c144ec4f3cbdf098
Marc-Schuster <marc_schuster@t-online.de>
2020-01-15 13:01:19 +0100

    new functionality waiting 3 seconds after answerphase before new round starts

--- Dixit/Assets/Scripts/GameManager.cs
+++ Dixit/Assets/Scripts/GameManager.cs
@@ -294,10 +294,14 @@ public class GameManager : MonoBehaviour
         }
         else
         {
+            Debug.Log("prewait");
+            StartCoroutine(WaitSecondsThanCleanup());
+           /* Debug.Log("postwait");
             CleanUp();
             pm.CreateNewCardForPlayers();
-            nextQuestion = true;
-            //NextQuestion();
+            //should be called to wait for 3 seconds so that the players kann see which answer was the right one
+                nextQuestion = true;
+            //NextQuestion();*/
         }
 
     }
@@ -327,6 +331,24 @@ public class GameManager : MonoBehaviour
         questionSet.QuestionList[0].correctAnswer.AddOneAndShuffle(allCards);
         pm.BroadcastAnswers(allCards);
     }
+
+    /// <summary>
+    /// This Coroutine waits for 3 seconds before starting the cleanup of the last round, creating new player cards and setting nextQuestion=true so that a new round can start.
+    /// </summary>
+    /// <returns>A Coroutine does not return anything</returns>
+    IEnumerator WaitSecondsThanCleanup()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("waited");
+        CleanUp();
+        pm.CreateNewCardForPlayers();
+        //should be called to wait for 3 seconds so that the players kann see which answer was the right one
+        nextQuestion = true;
+        //NextQuestion();
+    }
+
+
+
 
 }
 
