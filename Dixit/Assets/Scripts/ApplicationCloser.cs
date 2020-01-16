68dca5fa21ab7c212ed5411b7e80feda692e682c
Marc-Schuster <marc_schuster@t-online.de>
2020-01-16 16:38:06 +0100

    add missing script

---
+++ Dixit/Assets/Scripts/ApplicationCloser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationCloser : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void QuitApp()
+    {
+        Application.Quit();
+
+    }
+
+}
